d48f8ce9a88b3c4aa6664a8dc76f3b71f4228efc
prakritiacharya <156447301+prakritiacharya@users.noreply.github.com>
2025-04-17 19:43:06 +0000

    sync

--- js.js
+++ js.js
@@ -1,6 +1,20 @@
-const burger = document.getElementById("burger");
-const sidebar = document.getElementById("sidebar");
+// const burger = document.getElementById("burger");
+// const sidebar = document.getElementById("sidebar");
 
-burger.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-});
+// burger.addEventListener("click", () => {
+//   sidebar.classList.toggle("open");
+// });
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("sidebar").style.width = "250px";
+  document.getElementById("burger").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("burger").style.marginLeft = "0";
+  document.body.style.backgroundColor = "white";
+}
